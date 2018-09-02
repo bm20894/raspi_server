693533bc910dc4f01f2e450edbccfc0d15f133c3
bm20894 <milesboz@me.com>
2018-09-02 15:39:24 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const http = require('http').createServer(handler);
+const fs = require('fs');
+const io = require('socket.io')(http);
+const player = require('./player');
+
+const port = process.env.PORT || 8080;
+http.listen(port);
+console.log('Running on http://localhost:' + port)
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/public/index.html', function(e, data) {
+    if (e) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data); //write data from index.html
+    return res.end();
+  });
+}
+
+io.sockets.on('connection', socket => {
+
+	socket.on('light', status => {
+		console.log(status)
+	})
+
+	socket.on('sound', () => {
+		// console.log('I need to play a sound now!')
+		player.play({ file: './public/intro.wav' });
+	})
+})
---
+++ player.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+const { exec } = require("child_process");
+
+process.on('SIGINT', () => {
+	console.log();
+	process.exit();
+});
+
+module.exports = { play, setVol };
+
+
+function setVol(vol) {
+	vol = vol || 500;
+	exec('amixer set PCM -- -' + vol, () => {
+	        console.log('Volume set to ' + vol);
+	});
+}
+// param = { file, vol }
+function play(param) {
+	let { file, vol } = param;
+	setVol(vol);
+	let f = file.split("/");
+	f = f[f.length-1];
+	console.log("Playing " + f);
+
+	exec("aplay --quiet " + file, (e, out, err) => {
+		if(e) return console.log("There was an error executing");
+		// console.log(`stdout: ${out}`);
+		// console.log(`stderr: ${err}`);
+	});
+}
